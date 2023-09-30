34dfe7b2bb3d20bc4858a7b07e2bdb8435b2dd88
Dumitru Hristov <dima.hristov@gmail.com>
2023-09-30 11:05:03 +0300

    02-006 Coroutines implementation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,8 +1,18 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
+/**
+ * 1. written in pure imperative fashion and is executed sequentially
+ * 2. implementation is much shorter
+ * 3. you don't have to think about freeing resources, since viewModelScope
+ * 4. don't need to manually specify which code should run on which thread, since Retrofit calls
+ * are main thread safe (libs are usually implemented this way) and Retrofit itself takes care
+ * to execute in a background thread
+ * */
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
@@ -10,5 +20,19 @@ class Perform2SequentialNetworkRequestsViewModel(
     fun perform2SequentialNetworkRequest() {
         // TODO: Exercise 1
         // switch to branch "coroutine_course_full" to see solution
+
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentVersions.last()
+                val featuresOfMostRecentVersion = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("network request failed")
+            }
+        }
+
     }
 }
