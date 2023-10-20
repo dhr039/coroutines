3b750128bdc7c720c9ea86231b364b8cd3b42b83
Dumitru Hristov <dima.hristov@gmail.com>
2023-10-20 21:42:13 +0300

    05-008

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,49 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures = recentVersions.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("network request failed")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val recentVersion = mockApi.getRecentAndroidVersions()
+
+            try {
+                val result = recentVersion.map { androidVersion ->
+                    async {
+                        mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                    }
+                }.awaitAll()
+                uiState.value = UiState.Success(result)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error(e.toString())
+            }
+
+        }
     }
 }
