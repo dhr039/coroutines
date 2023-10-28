6fe8bbd8ec64f21f6ccaa9dc9ac5857b9becd0ea
Dumitru Hristov <dima.hristov@gmail.com>
2023-10-28 10:43:08 +0300

    06 - 003 Extract retry logic into higher order function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -10,30 +10,62 @@ class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
+    /*this is the code before implementing a higher order function for retry:*/
+//    fun performNetworkRequest() {
+//        uiState.value = UiState.Loading
+//
+//        viewModelScope.launch {
+//            val numberOfRetries = 2
+//
+//            try {
+//                repeat(numberOfRetries) {
+//                    /*if getRecentAndroidVersions() fails, it will exit the repeat(){} block, that's why we need another try/catch*/
+//                    try {
+//                        loadRecentAndroidVersions()
+//                        /*in case of success we leave the repeat(){} block so that no unnecessary request are performed:*/
+//                        return@launch // leave the whole coroutine, not just the repeat
+//                    } catch (e: Exception) {
+//                        Timber.e(e)
+//                    }
+//                }
+//                /*we have to try another request after the repeat(){} block, to be able to propagate to the outer catch:*/
+//                loadRecentAndroidVersions()
+//            } catch (e: Exception) {
+//                Timber.e(e)
+//                uiState.value = UiState.Error(e.toString())
+//            }
+//        }
+//    }
+
+
+    /*this is after we implementing a higher order function for retry:*/
     fun performNetworkRequest() {
         uiState.value = UiState.Loading
 
         viewModelScope.launch {
             val numberOfRetries = 2
-
             try {
-                repeat(numberOfRetries) {
-                    /*if getRecentAndroidVersions() fails, it will exit the repeat(){} block, that's why we need another try/catch*/
-                    try {
-                        loadRecentAndroidVersions()
-                        /*in case of success we leave the repeat(){} block so that no unnecessary request are performed:*/
-                        return@launch // leave the whole coroutine, not just the repeat
-                    } catch (e: Exception) {
-                        Timber.e(e)
-                    }
+                retry(numberOfRetries) {
+                    loadRecentAndroidVersions()
                 }
-                /*we have to try another request after the repeat(){} block, to be able to propagate to the outer catch:*/
-                loadRecentAndroidVersions()
             } catch (e: Exception) {
                 Timber.e(e)
                 uiState.value = UiState.Error(e.toString())
             }
         }
+    }
+
+    /*we also want to return the result of the block() while still keeping it generic
+    * so we have to introduce a new Generic type <T> */
+    private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T): T {
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+        return block()
     }
 
     private suspend fun loadRecentAndroidVersions() {
