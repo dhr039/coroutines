042c249104a70ad372ddecf2d7741b6933d9adad
Dumitru Hristov <dima.hristov@gmail.com>
2023-10-28 09:59:20 +0300

    06 - 001 Implementing a timeout

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,57 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import timber.log.Timber
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
 
+//        usingWithTimeout(timeout)
+        usingWithTimeoutOrNull(timeout)
+    }
+
+    private fun usingWithTimeout(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(recentAndroidVersions)
+            } catch (timeoutCancellationException: TimeoutCancellationException) {
+                uiState.value = UiState.Error("Network request timed out")
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error(e.toString())
+            }
+        }
+    }
+
+    private fun usingWithTimeoutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                if (recentAndroidVersions != null) {
+                    uiState.value = UiState.Success(recentAndroidVersions)
+                } else {
+                    uiState.value = UiState.Error("network request timed out")
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error(e.toString())
+            }
+        }
     }
 
 }
