0253c6197e72b84ba1cd2e0031307b80a6ae1e40
Dumitru Hristov <dima.hristov@gmail.com>
2023-10-28 18:13:52 +0300

    06 - 005 Combining retries and timeout

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
@@ -1,7 +1,14 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase7
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
 
 class TimeoutAndRetryViewModel(
     private val api: MockApi = mockApi()
@@ -12,10 +19,62 @@ class TimeoutAndRetryViewModel(
         val numberOfRetries = 2
         val timeout = 1000L
 
-        // TODO: Exercise 3
-        // switch to branch "coroutine_course_full" to see solution
-
         // run api.getAndroidVersionFeatures(27) and api.getAndroidVersionFeatures(28) in parallel
+        val oreoFeatures = viewModelScope.async {
+            retryWithTimeout(numberOfRetries, timeout) {
+                api.getAndroidVersionFeatures(27)
+            }
+        }
+        val pieFeatures = viewModelScope.async {
+            retryWithTimeout(numberOfRetries, timeout) {
+                api.getAndroidVersionFeatures(28)
+            }
+        }
 
+        viewModelScope.launch {
+            try {
+                val versionFeatures = listOf(
+                    oreoFeatures,
+                    pieFeatures
+                ).awaitAll()
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("network request failed")
+            }
+        }
+
+    }
+
+    private suspend fun <T> retryWithTimeout(
+        numberOfRetries: Int,
+        timeout: Long,
+        block: suspend () -> T
+    ) = retry(numberOfRetries) {
+        withTimeout(timeout) {
+            block()
+        }
+    }
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelay = initialDelayMillis
+
+        repeat(numberOfRetries) {
+            try {
+                block()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+
+        return block()
     }
 }
