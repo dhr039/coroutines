4ca35d80d93d7f30d261aa296a2a3c757d478632
Dumitru Hristov <dima.hristov@gmail.com>
2023-10-30 18:29:25 +0200

    08 - 008 Perform calculation in several Coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
@@ -1,8 +1,11 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase12
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
@@ -17,24 +20,28 @@ class CalculationInSeveralCoroutinesViewModel(
     ) {
         uiState.value = UiState.Loading
 
-        var factorialResult = BigInteger.ZERO
-        val computationDuration = measureTimeMillis {
-            factorialResult =
-                factorialCalculator.calculateFactorial(
-                    factorialOf,
-                    numberOfCoroutines
-                )
+        viewModelScope.launch {
+            var factorialResult = BigInteger.ZERO
+            val computationDuration = measureTimeMillis {
+                factorialResult =
+                    factorialCalculator.calculateFactorial(
+                        factorialOf,
+                        numberOfCoroutines
+                    )
+            }
+
+            var resultString = ""
+            val stringConversionDuration = measureTimeMillis {
+                resultString = convertToString(factorialResult)
+            }
+
+            uiState.value =
+                UiState.Success(resultString, computationDuration, stringConversionDuration)
         }
 
-        var resultString = ""
-        val stringConversionDuration = measureTimeMillis {
-            resultString = convertToString(factorialResult)
-        }
-
-        uiState.value =
-            UiState.Success(resultString, computationDuration, stringConversionDuration)
     }
 
-    // TODO: execute on background thread
-    private fun convertToString(number: BigInteger): String = number.toString()
+    // have to execute on background thread
+    private suspend fun convertToString(number: BigInteger): String =
+        withContext(Dispatchers.Default) { number.toString() }
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
@@ -2,29 +2,51 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 class FactorialCalculator(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
-    fun calculateFactorial(
+//    fun calculateFactorial(
+//        factorialOf: Int,
+//        numberOfCoroutines: Int
+//    ): BigInteger {
+//
+//        // TODO: create sub range list *on background thread*
+//        val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
+//
+//
+//        // TODO: calculate factorial of each subrange in separate coroutine
+//        // use calculateFactorialOfSubRange(subRange) therefore
+//
+//
+//        // TODO: create factorial result by multiplying all sub-results and return this
+//        // result
+//
+//        return BigInteger.ZERO
+//    }
+
+    suspend fun calculateFactorial(
         factorialOf: Int,
         numberOfCoroutines: Int
     ): BigInteger {
-
-        // TODO: create sub range list *on background thread*
-        val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
-
-
-        // TODO: calculate factorial of each subrange in separate coroutine
-        // use calculateFactorialOfSubRange(subRange) therefore
-
-
-        // TODO: create factorial result by multiplying all sub-results and return this
-        // result
-
-        return BigInteger.ZERO
+        return withContext(defaultDispatcher) {
+            val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
+            subRanges.map { subRange ->
+                async {
+                    calculateFactorialOfSubRange(subRange)
+                }
+            }.awaitAll()
+                .fold(BigInteger.ONE, { acc, element ->
+                    ensureActive()
+                    acc.multiply(element)
+                })
+        }
     }
 
     // TODO: execute on background thread
