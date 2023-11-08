976dc94dd6641eba47d6e4618e100d159bd4e528
Dumitru Hristov <dima.hristov@gmail.com>
2023-11-08 20:31:24 +0200

    09 - 014 Continue Coroutine execution when the user leaves the screen

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
@@ -3,6 +3,9 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class AndroidVersionRepository(
     private var database: AndroidVersionDao,
@@ -15,10 +18,24 @@ class AndroidVersionRepository(
     }
 
     suspend fun loadAndStoreRemoteAndroidVersions(): List<AndroidVersion> {
-        return emptyList()
+
+        return scope.async {
+            val recentVersions = api.getRecentAndroidVersions()
+            Timber.d("recent android versions loaded")
+
+
+            for (recentVersion in recentVersions) {
+                Timber.d("insert recent version $recentVersion into db")
+                database.insert(recentVersion.mapToEntity())
+            }
+            recentVersions
+        }.await()
+
     }
 
     fun clearDatabase() {
-
+        scope.launch {
+            database.clear()
+        }
     }
 }
