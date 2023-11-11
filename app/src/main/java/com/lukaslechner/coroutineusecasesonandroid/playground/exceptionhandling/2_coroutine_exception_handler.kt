2d5ddedbe5ad3e3d4cae092e1bcdc1bced59d997
Dumitru Hristov <dima.hristov@gmail.com>
2023-11-11 10:22:28 +0200

    11 - 003 Coroutine Exception Handler

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/exceptionhandling/2_coroutine_exception_handler.kt
@@ -0,0 +1,30 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.exceptionhandling
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+
+/**
+ * You should install an exception handler only in the scope or in a TOP level coroutine,
+ * in a nested(child) coroutine it will not have any effect.
+ * CancellationException will also not get handled by the coroutine exception handler.
+ * */
+fun main() {
+
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        println("caught $throwable in CoroutineExceptionHandler")
+    }
+
+    /** or could pass exceptionHandler to launch:  launch(exceptionHandler) {...}
+     * if passing to both, then only the handler passed to launch will be called.
+     * */
+    val scope = CoroutineScope(Job() + exceptionHandler)
+
+    scope.launch {
+        throw RuntimeException()
+    }
+
+    Thread.sleep(100)
+}
